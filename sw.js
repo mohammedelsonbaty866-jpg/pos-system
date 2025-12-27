48fd0e8fd0595a9a8bc5f1e909a4bcd0bc1149cc
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-27 13:07:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,45 @@
-const CACHE_NAME = "pos-cache-v1";
+const CACHE_NAME = "pos-system-v1";
+
 const FILES_TO_CACHE = [
   "./",
   "./index.html",
   "./manifest.json",
+  "./core.js",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
+/* تثبيت Service Worker */
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
   );
+  self.skipWaiting();
 });
 
+/* تفعيل Service Worker */
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+/* جلب الملفات (أوفلاين) */
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(
-      response => response || fetch(event.request)
-    )
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
   );
 });
