70874d61872ff2acb903ba81b74004472d601e4d
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-27 13:24:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,45 +1,26 @@
-const CACHE_NAME = "pos-system-v1";
+const CACHE_NAME = "pos-cache-v1";
 
-const FILES_TO_CACHE = [
+const FILES = [
   "./",
   "./index.html",
-  "./manifest.json",
   "./core.js",
+  "./manifest.json",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
-/* تثبيت Service Worker */
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
+self.addEventListener("install", e=>{
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache=>{
+      return cache.addAll(FILES);
     })
   );
-  self.skipWaiting();
 });
 
-/* تفعيل Service Worker */
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-/* جلب الملفات (أوفلاين) */
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
+self.addEventListener("fetch", e=>{
+  e.respondWith(
+    caches.match(e.request).then(res=>{
+      return res || fetch(e.request);
     })
   );
 });
