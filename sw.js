1a2fd6fc98d92e04de9397f69e73802e1cf40abf
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-27 13:36:44 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,17 @@
-const CACHE_NAME = "pos-cache-v1";
-
-const FILES = [
-  "./",
-  "./index.html",
-  "./core.js",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
+const CACHE="pos-v1";
+const FILES=[
+"./",
+"./index.html",
+"./core.js",
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
 ];
 
-self.addEventListener("install", e=>{
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache=>{
-      return cache.addAll(FILES);
-    })
-  );
+self.addEventListener("install",e=>{
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES)));
 });
 
-self.addEventListener("fetch", e=>{
-  e.respondWith(
-    caches.match(e.request).then(res=>{
-      return res || fetch(e.request);
-    })
-  );
+self.addEventListener("fetch",e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
