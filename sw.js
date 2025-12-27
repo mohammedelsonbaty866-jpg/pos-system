8c02fa1a11cb10dbc203b33330450fa01af40b70
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-27 18:55:22 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,43 @@
-const CACHE_NAME = "pos-cache-v1";
-const FILES = [
+const CACHE_NAME = "pos-cache-v3"; // غير الرقم كل تعديل
+const FILES_TO_CACHE = [
   "./",
   "./index.html",
-  "./reports.html",
-  "./manifest.json",
+  "./core.js",
   "./icon-192.png",
   "./icon-512.png"
 ];
 
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+// تثبيت
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+// تفعيل + حذف القديم
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+// جلب الملفات
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(res => {
+        const clone = res.clone();
+        caches.open(CACHE_NAME).then(c => c.put(event.request, clone));
+        return res;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
