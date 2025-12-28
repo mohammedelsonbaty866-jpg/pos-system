18bfd57a611565a007bb0f94f9c7e78103c15ae4
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-28 22:03:18 +0200

    Delete auth.js

--- auth.js
+++
@@ -1,9 +0,0 @@
-function login(){
-  if(username.value==="admin" && password.value==="1234"){
-    loginScreen.classList.remove("active");
-    app.classList.add("active");
-    renderProducts();
-  }else{
-    alert("بيانات غير صحيحة");
-  }
-}
