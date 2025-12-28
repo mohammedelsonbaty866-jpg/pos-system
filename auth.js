df14c02d0784cd1b295d5f8cd05ebf94359a0dd5
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-28 13:08:17 +0200

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,8 +1,5 @@
 function login(){
-  const u = username.value;
-  const p = password.value;
-
-  if(u==="admin" && p==="1234"){
+  if(username.value==="admin" && password.value==="1234"){
     loginScreen.classList.remove("active");
     app.classList.add("active");
     renderProducts();
