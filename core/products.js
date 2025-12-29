0732b0ad033ea2362de9e940b4eba32bdfb41a23
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-29 12:35:26 +0200

    Update products.js

--- core/products.js
+++ core/products.js
@@ -1,1 +1,23 @@
+const Products = {
+  key: "pos_products",
 
+  getAll() {
+    return Storage.get(this.key) || [];
+  },
+
+  save(list) {
+    Storage.set(this.key, list);
+  },
+
+  add(p) {
+    const products = this.getAll();
+    products.push(p);
+    this.save(products);
+  },
+
+  update(index, data) {
+    const products = this.getAll();
+    products[index] = {...products[index], ...data};
+    this.save(products);
+  }
+};
