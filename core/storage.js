5e15596a33312b3a5ebfbe1173016398b590c2bb
mohammedelsonbaty866-jpg <mohammedelsonbaty866@gmail.com>
2025-12-29 11:50:19 +0200

    Create storage.js

---
+++ core/storage.js
@@ -0,0 +1,14 @@
+const Storage = {
+  set(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  },
+
+  get(key) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : null;
+  },
+
+  remove(key) {
+    localStorage.removeItem(key);
+  }
+};
